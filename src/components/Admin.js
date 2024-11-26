ea5c8e897c901a4c0239ea68d546bf4d8b7b920e
kaykyjuvenal <neguito.juvenal@gmail.com>
2024-11-26 01:03:51 -0300

    Subindo atualização

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -76,13 +76,6 @@ function Admin() {
     } catch (error) {
       setError(error.message);
     }
-    return <div>
-    <h2>Endereço:</h2>
-    <p>Rua: {data.logradouro}</p>
-    <p>Bairro: {data.bairro}</p>
-    <p>Cidade: {data.localidade}</p>
-    <p>Estado: {data.uf}</p>
-  </div>
   };
   
   // Função para adicionar médico
